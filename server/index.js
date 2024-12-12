d8510ba7b8981828fd1e408264a3472697cc3f7a
Elva Osorio <osorioelva@gmail.com>
2024-12-11 18:36:01 -0800

    first changes in a min, for server and env

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ Model.knex(knex);
 const app = express();
 
 const redirect_uri =
-  process.env.REDIRECT_URI || "http://localhost:3001/callback";
+  process.env.REDIRECT_URI || "http://localhost:3001/api/callback";
 
 app.use(bodyParser.json());
 app.get("/login", function(req, res) {
@@ -121,5 +121,7 @@ app.use((error, request, response, next) => {
 });
 
 
-const server = http.createServer(app).listen(process.env.PORT || 3001);
-console.log("Listening on port %d", server.address().port); // eslint-disable-line no-console
+//const server = http.createServer(app).listen(process.env.PORT || 3001);
+module.exports = app;
+
+//console.log("Listening on port %d", server.address().port); // eslint-disable-line no-console
